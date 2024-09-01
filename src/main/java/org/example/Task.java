03d19fc2444f5e23aefea2c1c9fadaf302065650
ilovechocolates05 <109774245+ilovechocolates05@users.noreply.github.com>
2024-09-01 13:31:48 -0500

    Added setter methods

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -1,24 +1,26 @@
 package org.example;
 
 public class Task {
+    //private fields to store task properties
     private String taskName;
     private String taskDescription;
     private boolean isCompleted;
 
+    //constructor to create a Task with only a name
     public Task (String taskName) {
         this.taskName = taskName;
         this.taskDescription = "";
         this.isCompleted = false;
     }
 
+    //constructor to create a Task with a name and description
     public Task (String taskName, String taskDescription) {
         this.taskName = taskName;
         this.taskDescription = taskDescription;
         this.isCompleted = false;
     }
 
-    //Getter
-
+    //Getter methods
     public String getName() {
         return taskName;
     }
@@ -30,4 +32,17 @@ public class Task {
     public boolean isCompleted() {
         return isCompleted;
     }
+
+    //Setter methods
+    public void setName(String taskName) {
+        this.taskName = taskName;
+    }
+    public void setDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
 }

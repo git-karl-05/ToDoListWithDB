468bfd8cd5489d96ca3d54042c0c282e0fe6617d
ilovechocolates05 <109774245+ilovechocolates05@users.noreply.github.com>
2024-09-01 13:22:06 -0500

    Added constructor  and getter methods

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -1,4 +1,33 @@
 package org.example;
 
 public class Task {
+    private String taskName;
+    private String taskDescription;
+    private boolean isCompleted;
+
+    public Task (String taskName) {
+        this.taskName = taskName;
+        this.taskDescription = "";
+        this.isCompleted = false;
+    }
+
+    public Task (String taskName, String taskDescription) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.isCompleted = false;
+    }
+
+    //Getter
+
+    public String getName() {
+        return taskName;
+    }
+
+    public String getDescription() {
+        return taskDescription;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
 }
